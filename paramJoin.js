23e5f062f1c26a9344a3561c19c655cdf96b4b06
Steve Sharp <40205362+sharpsteve@users.noreply.github.com>
2019-09-04 08:05:57 -0400

    Update paramJoin.js

--- paramJoin.js
+++ paramJoin.js
@@ -1,4 +1,5 @@
 // Since we need to append the parameters of the request differently from the path
+// https://howchoo.com/g/nwywodhkndm/how-to-turn-an-object-into-query-string-parameters-in-javascript
 function paramJoin(path, params) {
 	var queryString = Object.keys(params).map(function(key) {
 		return key + '=' + params[key]
