45960afc3227c2afa7239c5f840b71f84e13bdff
Steve Sharp <40205362+sharpsteve@users.noreply.github.com>
2019-09-04 14:49:49 -0400

    Update pathJoin.js

--- pathJoin.js
+++ pathJoin.js
@@ -1,11 +1,12 @@
+// This is a useful function to have
 function pathJoin() {
-	// trim slashes from arguments, prefix a slash to the beginning of each, re-join (ignores empty parameters)
-	var args = Array.prototype.slice.call(arguments, 0)
-	var nonempty = args.filter(function (arg, idx, arr) {
-		return typeof (arg) != 'undefined'
-	})
-	var trimmed = nonempty.map(function (arg, idx, arr) {
-		return '/' + String(arg).replace(new RegExp('^/+|/+$'), '')
-	})
-	return trimmed.join('')
+    // trim slashes from arguments, prefix a slash to the beginning of each, re-join (ignores empty parameters)
+    var args = Array.prototype.slice.call(arguments, 0)
+    var nonempty = args.filter(function (arg, idx, arr) {
+        return typeof (arg) != 'undefined'
+    })
+    var trimmed = nonempty.map(function (arg, idx, arr) {
+        return '/' + String(arg).replace(new RegExp('^/+|/+$'), '')
+    })
+    return trimmed.join('')
 }
