c9972402df4e598c1761acccb959ce21014fd93e
Steve Sharp <40205362+sharpsteve@users.noreply.github.com>
2019-09-06 13:05:34 -0400

    Update areDatesEqual.js

--- areDatesEqual.js
+++ areDatesEqual.js
@@ -3,5 +3,5 @@ function areDatesEqual(date1, date2) {
     var date1Converted = new Date(date1);
     var date2Converted = new Date(date2);
 
-    return date1Converted.getTime() == date2Converted.getTime();
+    return date1Converted.getTime() === date2Converted.getTime();
 }
